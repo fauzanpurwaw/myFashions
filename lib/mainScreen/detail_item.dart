6aad536e6fd061ee2be6af4fa5b656b0afb0b0ba
fauzanpurwaw <fauzanpurwaw@gmail.com>
2023-12-14 11:31:50 +0700

    [update detail item]

--- lib/mainScreen/detail_item.dart
+++ lib/mainScreen/detail_item.dart
@@ -135,7 +135,7 @@ class _ScrDetailItemState extends State<ScrDetailItem> {
                                       MainAxisAlignment.spaceAround,
                                   children: [
                                     Container(
-                                      width: 80,
+                                      width: 90,
                                       height: 30,
                                       padding: const EdgeInsets.symmetric(
                                           horizontal: 10),
@@ -185,82 +185,112 @@ class _ScrDetailItemState extends State<ScrDetailItem> {
                           ),
                         ],
                       ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //   crossAxisAlignment: CrossAxisAlignment.center,
-                      //   children: [
-                      //     Container(
-                      //       height: 80,
-                      //       child: const Column(
-                      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      //         crossAxisAlignment: CrossAxisAlignment.start,
-                      //         children: [
-                      //           Text(
-                      //             "Size",
-                      //             style: TextStyle(
-                      //                 fontSize: 20,
-                      //                 fontWeight: FontWeight.bold),
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //     Wrap(
-                      //       children: [
-                      //         Container(
-                      //           height: 80,
-                      //           child: Column(
-                      //             mainAxisAlignment:
-                      //                 MainAxisAlignment.spaceAround,
-                      //             children: [
-                      //               Container(
-                      //                 width: 80,
-                      //                 height: 30,
-                      //                 padding: const EdgeInsets.symmetric(
-                      //                     horizontal: 10),
-                      //                 decoration: const BoxDecoration(
-                      //                     borderRadius: BorderRadius.all(
-                      //                         Radius.circular(25)),
-                      //                     color: Color.fromARGB(
-                      //                         255, 223, 222, 222)),
-                      //                 child: Row(
-                      //                   mainAxisAlignment:
-                      //                       MainAxisAlignment.spaceBetween,
-                      //                   children: [
-                      //                     InkWell(
-                      //                       onTap: () {
-                      //                         setState(() {
-                      //                           if (stockCounter != 1) {
-                      //                             stockCounter--;
-                      //                           }
-                      //                         });
-                      //                       },
-                      //                       child: Text("-"),
-                      //                     ),
-                      //                     Text(stockCounter.toString()),
-                      //                     InkWell(
-                      //                       onTap: () {
-                      //                         setState(() {
-                      //                           stockCounter++;
-                      //                         });
-                      //                       },
-                      //                       child: Text("+"),
-                      //                     ),
-                      //                   ],
-                      //                 ),
-                      //               ),
-                      //               const Text(
-                      //                 "available in stok",
-                      //                 style: TextStyle(
-                      //                     fontSize: 12,
-                      //                     fontWeight: FontWeight.bold),
-                      //               )
-                      //             ],
-                      //           ),
-                      //         )
-                      //       ],
-                      //     ),
-                      //   ],
-                      // )
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 90,
+                            margin: EdgeInsets.only(top: 20),
+                            width: 100,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Color",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        width: 30,
+                                        height: 30,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          border: Border.all(
+                                              color: Colors.grey, width: 2),
+                                          color: Colors.amber,
+                                        ),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        width: 30,
+                                        height: 30,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          border: Border.all(
+                                              color: Colors.grey, width: 2),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        width: 30,
+                                        height: 30,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          border: Border.all(
+                                              color: Colors.grey, width: 2),
+                                          color: Colors.black,
+                                        ),
+                                        child: Icon(
+                                          Icons.check,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 90,
+                            margin: EdgeInsets.only(top: 20),
+                            width: MediaQuery.of(context).size.width - 70,
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Description",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Get a little lift from these Sam Edelman sandals featuring ruched straps and leather lace-up ties, while a braided jute sole makes a fresh statement for summer.",
+                                  maxLines: 6,
+                                  overflow: TextOverflow.ellipsis,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
@@ -268,6 +298,63 @@ class _ScrDetailItemState extends State<ScrDetailItem> {
             ],
           ),
         ]),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          width: MediaQuery.of(context).size.width,
+          height: 90,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      Text(
+                        "\$198.00",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    highlightColor: Colors.white,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      width: 180,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.shopping_bag_outlined,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Add to cart",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
